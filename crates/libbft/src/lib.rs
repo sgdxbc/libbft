864d6c715cd3c2503b0a5770bbb8eb49543b1e81
Sun Guangda <sung@comp.nus.edu.sg>
2026-05-04 12:10:34 +0800

    feat(libbft): null service

--- crates/libbft/src/lib.rs
+++ crates/libbft/src/lib.rs
@@ -5,4 +5,5 @@ pub mod hotstuff; // should called hot_stuff, but that looks really really weird
 pub mod kv;
 pub mod narwhal;
 pub mod network;
+pub mod null;
 pub mod pbft;
---
+++ crates/libbft/src/null.rs
@@ -0,0 +1,41 @@
+use tokio_util::sync::CancellationToken;
+use tracing::warn;
+
+use crate::event::{Emit, EventChannel};
+
+pub mod events {
+    use tokio::sync::oneshot;
+
+    use crate::{common::Payload, event::Event};
+
+    pub struct Execute;
+    impl Event for Execute {
+        type Value = (Vec<Payload>, oneshot::Sender<Option<Vec<Payload>>>);
+    }
+}
+
+pub struct Null {
+    op: EventChannel<events::Execute>,
+}
+
+impl Null {
+    #[allow(clippy::new_without_default)]
+    pub fn new() -> Self {
+        Self {
+            op: EventChannel::new(None),
+        }
+    }
+
+    pub fn register(&self, mut emit_op: impl Emit<events::Execute>) {
+        emit_op.install(self.op.sender());
+    }
+
+    pub async fn run(&mut self, token: &CancellationToken) {
+        while let Some(Some(((_ops, tx), _span))) = token.run_until_cancelled(self.op.recv()).await
+        {
+            if tx.send(None).is_err() {
+                warn!("failed to send response to client");
+            }
+        }
+    }
+}
